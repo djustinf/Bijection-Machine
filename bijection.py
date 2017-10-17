62c159469a41a2844a9c86e792a9f61910b2d984
djustinf <djustinf@gmail.com>
2017-10-16 22:20:27 -0700

    Included an example at the bottom of the file for clarity

--- bijection.py
+++ bijection.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import operator
 
 def bijection_machine(partition, A, B):
     A = [Counter(x) for x in A]
@@ -11,7 +12,7 @@ def bijection_machine(partition, A, B):
         # beta() to either add or remove smallest disease present
         alpha_and_beta(diseases, B, partition)
         # if there are no diseases listed after beta(), we know it is a fixed point
-        if (not diseases): return +partition
+        if (not diseases): return sorted((+partition).elements())
         # f^-1() to convert diseases in B to diseases in A
         f_and_f_inverse(diseases, B, A, partition)
         # alpha() to either add or remove smallest disease present
@@ -31,6 +32,9 @@ def f_and_f_inverse(diseases, fromList, toList, partition):
         partition.subtract(fromList[disease])
         partition.update(toList[disease])
 
-A = [[2], [3], [4], [6], [8], [9], [10]]
-B = [[1,1], [3], [2,2], [6], [4,4], [9], [5,5]]
-partition = [1, 1, 1, 1, 5, 5]
+# USAGE: Enter 2 lists of diseases, A and B (should be lists of lists) and an integer partition (list of integers)
+# Below is an example input for which the output should be the partition [4, 10]
+# A = [[2], [3], [4], [6], [8], [9], [10]]
+# B = [[1,1], [3], [2,2], [6], [4,4], [9], [5,5]]
+# partition = [1, 1, 1, 1, 5, 5]
+# print(bijection_machine(partition, A, B))
